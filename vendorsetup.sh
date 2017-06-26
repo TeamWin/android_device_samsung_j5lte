TeamWin/android_device_samsung_j5ltechn

j5: Reconfigure for Omni

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_j5ltechn-${i}
+	add_lunch_combo omni_j5ltechn-${i}
 done

TeamWin/android_device_samsung_j5lte

vendorsetup.sh
@@ -1,3 +1,3 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_j5lte-${i}
+	add_lunch_combo omni_j5lte-${i}
 done